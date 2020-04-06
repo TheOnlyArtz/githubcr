7dbf393ede7d79602370f16104a1084308fc7f5d
callcraft456@gmail.com <callcraft456@gmail.com>
2020-04-06 13:06:44 +0300

    REST initialization with Events and Gists

--- src/githubcr/rest.cr
+++ src/githubcr/rest.cr
@@ -1,9 +1,23 @@
 require "http/client"
+require "./mappings/*"
 
 module GitHub
   # This module enables GitHub.cr to interact with the
   # GitHub API through HTTP requests
   module REST
+    # This module is specifically for interacting with
+    # the Events endpoint GitHub offer us to use.
+    # see [GitHub Events endpoints](https://developer.github.com/v3/activity/events/)
+    # TODO ↑
+    module Events
 
+    end
+
+    # This module is specifically for interacting with
+    # the Gists endpoints GitHub offers us to use.
+    # see [GitHub Gists endpoints](https://developer.github.com/v3/gists/)
+    module Gists
+
+    end
   end
 end
