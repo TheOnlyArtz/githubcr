bb35d08381b7f059ea900ac4f097de00c10442b4
TheOnlyArtz <callcraft456@gmail.com>
2020-04-06 12:24:34 +0300

    Initialize the project

---
+++ spec/githubcr_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Githubcr do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/githubcr.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Githubcr`
+module Githubcr
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
