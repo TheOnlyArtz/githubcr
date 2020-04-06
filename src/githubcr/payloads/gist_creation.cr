75b4e27fadb56a016269e261dde1fc0ee512fb7d
callcraft456@gmail.com <callcraft456@gmail.com>
2020-04-06 15:45:35 +0300

    Gist creation payloads

---
+++ src/githubcr/payloads/gist_creation.cr
@@ -0,0 +1,20 @@
+# Represents a file
+struct GistCreationFilePayload
+  property content
+  def initialize(@content : String)
+  end
+end
+
+# Represents gist creation payload
+struct GistCreationPayload
+  def initialize(@description : String, @public : Bool, @files : Hash(String, GistCreationFilePayload))
+  end
+
+  def to_h
+    {
+      "description" => @description,
+      "public" => @public,
+      "files" => @files
+    }
+  end
+end
