489687058efd1ea8a6ecccb25547f9ca8f5cb582
callcraft456@gmail.com <callcraft456@gmail.com>
2020-04-06 22:43:55 +0300

    Better code generation

--- mapping_generator/index.js
+++ mapping_generator/index.js
@@ -12,7 +12,7 @@ String.prototype.replaceAt=function(index, replacement) {
 /**
 * @param {Object} payload
 **/
-function generate(payload, name, indentationLevel = 2) {
+function generate(payload, name, indentationLevel = 2, root = true) {
   if (!payload)
   {
     return "";
@@ -28,16 +28,17 @@ function generate(payload, name, indentationLevel = 2) {
   let i = 0;
   keys.forEach(key => {
     if (payload[key] == null) {
-      newKey = key.replaceAt(0, key[0].toUpperCase());
+      let newKey = key.replaceAt(0, key[0].toUpperCase());
       code.push("  ".repeat(indentationLevel) + `${key}: <CHANGE_ME>`);
     } else if (typeof(payload[key]) == "object" && payload[key].length == undefined){
-      newKey = key.replaceAt(0, key[0].toUpperCase());
-      children.push(generate(payload[key], newKey, indentationLevel));
+      let newKey = key.replaceAt(0, key[0].toUpperCase());
+      newKey = root ? newKey : name + newKey;
+      children.push(generate(payload[key], newKey, indentationLevel, false));
       code.push("  ".repeat(indentationLevel) + `${key}: ${newKey}`);
     } else if (typeof(payload[key]) == "object" && payload[key].length != undefined) {
       // ARRAY
-      newKey = key.replaceAt(0, key[0].toUpperCase());
-      console.log(generate(payload[key][0], newKey, indentationLevel))
+      let newKey = key.replaceAt(0, key[0].toUpperCase());
+      children.push(generate(payload[key][0], newKey, indentationLevel, false));
       code.push("  ".repeat(indentationLevel) + `${key}: Array(${newKey})`);
     } else {
       code.push("  ".repeat(indentationLevel) + `${key}: ${keyMappings[typeof(payload[key])]}`);
@@ -61,35 +62,102 @@ function generate(payload, name, indentationLevel = 2) {
 }
 
 const JSON = {
-  "sha": "7638417db6d59f3c431d3e1f261cc637155684cd",
-  "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/7638417db6d59f3c431d3e1f261cc637155684cd",
+  "sha": "388e61b6c707367aca2b9737766835f4cbe2cbb4",
+  "node_id": "MDY6Q29tbWl0NTY1MTAyMTk6Mzg4ZTYxYjZjNzA3MzY3YWNhMmI5NzM3NzY2ODM1ZjRjYmUyY2JiNA==",
+  "commit": {
+    "author": {
+      "name": "SinisterRectus",
+      "email": "sinister.rectus@gmail.com",
+      "date": "2020-03-06T17:09:36Z"
+    },
+    "committer": {
+      "name": "SinisterRectus",
+      "email": "sinister.rectus@gmail.com",
+      "date": "2020-03-06T17:09:36Z"
+    },
+    "message": "Fixes ratelimit header lookup",
+    "tree": {
+      "sha": "4dc8c11d8c1a0e588a7a05ce103fcf5ca3e63a0d",
+      "url": "https://api.github.com/repos/SinisterRectus/Discordia/git/trees/4dc8c11d8c1a0e588a7a05ce103fcf5ca3e63a0d"
+    },
+    "url": "https://api.github.com/repos/SinisterRectus/Discordia/git/commits/388e61b6c707367aca2b9737766835f4cbe2cbb4",
+    "comment_count": 0,
+    "verification": {
+      "verified": false,
+      "reason": "unsigned",
+      "signature": null,
+      "payload": null
+    }
+  },
+  "url": "https://api.github.com/repos/SinisterRectus/Discordia/commits/388e61b6c707367aca2b9737766835f4cbe2cbb4",
+  "html_url": "https://github.com/SinisterRectus/Discordia/commit/388e61b6c707367aca2b9737766835f4cbe2cbb4",
+  "comments_url": "https://api.github.com/repos/SinisterRectus/Discordia/commits/388e61b6c707367aca2b9737766835f4cbe2cbb4/comments",
   "author": {
-    "date": "2014-11-07T22:01:45Z",
-    "name": "Monalisa Octocat",
-    "email": "octocat@github.com"
+    "login": "SinisterRectus",
+    "id": 11720541,
+    "node_id": "MDQ6VXNlcjExNzIwNTQx",
+    "avatar_url": "https://avatars1.githubusercontent.com/u/11720541?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/SinisterRectus",
+    "html_url": "https://github.com/SinisterRectus",
+    "followers_url": "https://api.github.com/users/SinisterRectus/followers",
+    "following_url": "https://api.github.com/users/SinisterRectus/following{/other_user}",
+    "gists_url": "https://api.github.com/users/SinisterRectus/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/SinisterRectus/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/SinisterRectus/subscriptions",
+    "organizations_url": "https://api.github.com/users/SinisterRectus/orgs",
+    "repos_url": "https://api.github.com/users/SinisterRectus/repos",
+    "events_url": "https://api.github.com/users/SinisterRectus/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/SinisterRectus/received_events",
+    "type": "User",
+    "site_admin": false
   },
   "committer": {
-    "date": "2014-11-07T22:01:45Z",
-    "name": "Monalisa Octocat",
-    "email": "octocat@github.com"
-  },
-  "message": "added readme, because im a good github citizen",
-  "tree": {
-    "url": "https://api.github.com/repos/octocat/Hello-World/git/trees/691272480426f78a0138979dd3ce63b77f706feb",
-    "sha": "691272480426f78a0138979dd3ce63b77f706feb"
+    "login": "SinisterRectus",
+    "id": 11720541,
+    "node_id": "MDQ6VXNlcjExNzIwNTQx",
+    "avatar_url": "https://avatars1.githubusercontent.com/u/11720541?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/SinisterRectus",
+    "html_url": "https://github.com/SinisterRectus",
+    "followers_url": "https://api.github.com/users/SinisterRectus/followers",
+    "following_url": "https://api.github.com/users/SinisterRectus/following{/other_user}",
+    "gists_url": "https://api.github.com/users/SinisterRectus/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/SinisterRectus/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/SinisterRectus/subscriptions",
+    "organizations_url": "https://api.github.com/users/SinisterRectus/orgs",
+    "repos_url": "https://api.github.com/users/SinisterRectus/repos",
+    "events_url": "https://api.github.com/users/SinisterRectus/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/SinisterRectus/received_events",
+    "type": "User",
+    "site_admin": false
   },
   "parents": [
     {
-      "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/1acc419d4d6a9ce985db7be48c6349a0475975b5",
-      "sha": "1acc419d4d6a9ce985db7be48c6349a0475975b5"
+      "sha": "8ad5334d8fe27223383cc98a1941b31f0fa113e8",
+      "url": "https://api.github.com/repos/SinisterRectus/Discordia/commits/8ad5334d8fe27223383cc98a1941b31f0fa113e8",
+      "html_url": "https://github.com/SinisterRectus/Discordia/commit/8ad5334d8fe27223383cc98a1941b31f0fa113e8"
     }
   ],
-  "verification": {
-    "verified": false,
-    "reason": "unsigned",
-    "signature": null,
-    "payload": null
-  }
+  "stats": {
+    "total": 8,
+    "additions": 4,
+    "deletions": 4
+  },
+  "files": [
+    {
+      "sha": "ff06915c2aa905d35c5072246a16cfbe6109ca29",
+      "filename": "libs/client/API.lua",
+      "status": "modified",
+      "additions": 4,
+      "deletions": 4,
+      "changes": 8,
+      "blob_url": "https://github.com/SinisterRectus/Discordia/blob/388e61b6c707367aca2b9737766835f4cbe2cbb4/libs/client/API.lua",
+      "raw_url": "https://github.com/SinisterRectus/Discordia/raw/388e61b6c707367aca2b9737766835f4cbe2cbb4/libs/client/API.lua",
+      "contents_url": "https://api.github.com/repos/SinisterRectus/Discordia/contents/libs/client/API.lua?ref=388e61b6c707367aca2b9737766835f4cbe2cbb4",
+      "patch": "@@ -187,15 +187,15 @@ function API:commit(method, url, req, payload, retries)\n \tend\n \n \tfor i, v in ipairs(res) do\n-\t\tres[v[1]] = v[2]\n+\t\tres[v[1]:lower()] = v[2]\n \t\tres[i] = nil\n \tend\n \n-\tif res['X-RateLimit-Remaining'] == '0' then\n-\t\tdelay = max(1000 * res['X-RateLimit-Reset-After'], delay)\n+\tif res['x-ratelimit-remaining'] == '0' then\n+\t\tdelay = max(1000 * res['x-ratelimit-reset-after'], delay)\n \tend\n \n-\tlocal data = res['Content-Type'] == JSON and decode(msg, 1, null) or msg\n+\tlocal data = res['content-type'] == JSON and decode(msg, 1, null) or msg\n \n \tif res.code < 300 then\n "
+    }
+  ]
 }
 
-console.log(generate(JSON, "Commit"));
+console.log(generate(JSON, "Commit", 2, true));
