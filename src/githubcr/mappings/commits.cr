8fb2e5abf97a151fc5a22299e0dd2363e4c9afbf
callcraft456@gmail.com <callcraft456@gmail.com>
2020-04-06 22:32:06 +0300

    Updated commits since GH docs were not updated

--- src/githubcr/mappings/commits.cr
+++ src/githubcr/mappings/commits.cr
@@ -1,33 +1,48 @@
-require "json"
-
 struct Parent
   JSON.mapping(
+    sha: String,
     url: String,
-    sha: String
+    html_url: String
   )
 end
 
 
-struct Author
+struct CommitFile
   JSON.mapping(
-    date: String,
-    name: String,
-    email: String
+    sha: String,
+    filename: String,
+    status: String,
+    additions: Int32,
+    deletions: Int32,
+    changes: Int32,
+    blob_url: String,
+    raw_url: String,
+    contents_url: String,
+    patch: String
   )
 end
 
-struct Committer
+
+struct CommitAuthor
   JSON.mapping(
-    date: String,
     name: String,
-    email: String
+    email: String,
+    date: String
+  )
+end
+
+struct CommitCommitter
+  JSON.mapping(
+    name: String,
+    email: String,
+    date: String
   )
 end
 
 struct Tree
   JSON.mapping(
-    url: String,
-    sha: String
+    sha: String,
+    url: String
   )
 end
 
@@ -35,20 +50,89 @@ struct Verification
   JSON.mapping(
     verified: Bool,
     reason: String,
-    signature: String,
-    payload: String
+    signature: String?,
+    payload: String?
+  )
+end
+
+struct CommitData
+  JSON.mapping(
+    author: CommitAuthor,
+    committer: CommitCommitter,
+    message: String,
+    tree: Tree,
+    url: String,
+    comment_count: Int32,
+    # verification: Verification TODO: look why this can't be serialized
+  )
+end
+
+struct Author
+  JSON.mapping(
+    login: String,
+    id: Int32,
+    node_id: String,
+    avatar_url: String,
+    gravatar_id: String,
+    url: String,
+    html_url: String,
+    followers_url: String,
+    following_url: String,
+    gists_url: String,
+    starred_url: String,
+    subscriptions_url: String,
+    organizations_url: String,
+    repos_url: String,
+    events_url: String,
+    received_events_url: String,
+    type: String,
+    site_admin: Bool
+  )
+end
+
+struct Committer
+  JSON.mapping(
+    login: String,
+    id: Int32,
+    node_id: String,
+    avatar_url: String,
+    gravatar_id: String,
+    url: String,
+    html_url: String,
+    followers_url: String,
+    following_url: String,
+    gists_url: String,
+    starred_url: String,
+    subscriptions_url: String,
+    organizations_url: String,
+    repos_url: String,
+    events_url: String,
+    received_events_url: String,
+    type: String,
+    site_admin: Bool
+  )
+end
+
+struct Stats
+  JSON.mapping(
+    total: Int32,
+    additions: Int32,
+    deletions: Int32
   )
 end
 
 struct Commit
   JSON.mapping(
     sha: String,
+    node_id: String,
+    commit: CommitData,
     url: String,
+    html_url: String,
+    comments_url: String,
     author: Author,
     committer: Committer,
-    message: String,
-    tree: Tree,
     parents: Array(Parent),
-    verification: Verification
+    stats: Stats,
+    files: Array(CommitFile)
   )
 end
