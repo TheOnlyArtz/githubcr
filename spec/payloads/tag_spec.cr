0ebdd367a3952a5f392968c12f54eebc260e77c2
callcraft456@gmail.com <callcraft456@gmail.com>
2020-04-07 18:01:04 +0300

    tag payload

---
+++ spec/payloads/tag_spec.cr
@@ -0,0 +1,27 @@
+require "../spec_helper.cr"
+
+describe TagPayload do
+  it "Should serialize" do
+    payload = TagPayload.new(
+      tag: "v0.0.1",
+      message: "Initial version\n",
+      object: "c3d0be41ecbe669545ee3e94d31ed9a4bc91ee3c",
+      type: "commit",
+      tagger: TagPayload::Tagger.new(
+        name: "Amit"
+      )
+    ).to_json
+
+    equal = {
+      "tag" => "v0.0.1",
+      "message" => "Initial version\n",
+      "object" => "c3d0be41ecbe669545ee3e94d31ed9a4bc91ee3c",
+      "type" => "commit",
+      "tagger" => {
+        "name" => "Amit"
+      }
+    }
+
+    payload.should eq(equal.to_json)
+  end
+end
---
+++ src/githubcr/payloads/tag.cr
@@ -0,0 +1,17 @@
+require "json"
+
+record TagPayload,
+  tag : String,
+  message : String,
+  object : String,
+  type : String,
+  tagger : TagPayload::Tagger? = nil do
+    include JSON::Serializable
+end
+
+record TagPayload::Tagger,
+  name : String? = nil,
+  email : String? = nil,
+  date : String? = nil do
+    include JSON::Serializable
+end
