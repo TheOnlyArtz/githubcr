7222827a0f3ec0946a19d8729429443a9f814998
callcraft456@gmail.com <callcraft456@gmail.com>
2020-04-07 18:30:07 +0300

    Tree payload

---
+++ spec/payloads/tree_spec.cr
@@ -0,0 +1,30 @@
+require "../spec_helper.cr"
+describe TreePayload do
+  it "Should serialize" do
+    payload = TreePayload.new(
+      tree: [
+        TreePayload::Tree.new(
+          path: "file.rb",
+          mode: "100644",
+          type: "blob",
+          sha: "44b4fc6d56897b048c772eb4087f854f46256132"
+        )
+      ],
+      base_tree: "9fb037999f264ba9a7fc6274d15fa3ae2ab98312"
+    ).to_json
+
+    equal = {
+      "tree" => [
+          {
+            "path" => "file.rb",
+            "mode" => "100644",
+            "type" => "blob",
+            "sha" => "44b4fc6d56897b048c772eb4087f854f46256132"
+          }
+      ],
+      "base_tree" => "9fb037999f264ba9a7fc6274d15fa3ae2ab98312"
+    }
+
+    payload.should eq(equal.to_json)
+  end
+end
---
+++ src/githubcr/payloads/tree.cr
@@ -0,0 +1,15 @@
+require "json"
+
+record TreePayload,
+  tree : Array(TreePayload::Tree),
+  base_tree : String? = nil do
+    include JSON::Serializable
+end
+
+record TreePayload::Tree,
+  path : String,
+  mode : String,
+  type : String,
+  sha : String do
+    include JSON::Serializable
+end
