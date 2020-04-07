7f392604bdbbc0c458d7d53b0143198120b2409f
callcraft456@gmail.com <callcraft456@gmail.com>
2020-04-07 18:14:44 +0300

    Tree mapping

---
+++ src/githubcr/mappings/trees.cr
@@ -0,0 +1,24 @@
+require "json"
+
+struct Tree
+end
+
+struct Tree::TreeData
+  JSON.mapping(
+    path: {type: String, setter: false},
+    mode: {type: String, setter: false},
+    type: {type: String, setter: false},
+    size: {type: Int32, setter: false},
+    sha: {type: String, setter: false},
+    url: {type: String, setter: false}
+  )
+end
+
+struct Tree
+  JSON.mapping(
+    sha: {type: String, setter: false},
+    url: {type: String, setter: false},
+    tree: {type: Array(TreeData), setter: false},
+    truncated: {type: Bool, setter: false}
+  )
+end
