9a6cedd10ed51423e6903669a123a86fe04ec904
callcraft456@gmail.com <callcraft456@gmail.com>
2020-04-07 16:01:29 +0300

    Better mappings generation

--- mapping_generator/mapping.js
+++ mapping_generator/mapping.js
@@ -28,13 +28,13 @@ function getNewKey(key, name, root, array = false) {
   return newKey;
 }
 
-function generate(payload, name, indentationLevel = 2, root = true) {
+function generate(payload, name, indentationLevel = 2, root = true, rootName) {
   if (!payload)
     return "";
 
   let keys = Object.keys(payload);
   let code = [
-    `struct ${name}`,
+    `struct ${!root ? rootName + "::" : ""}${name}`,
     "  JSON.mapping("
   ];
 
@@ -48,12 +48,12 @@ function generate(payload, name, indentationLevel = 2, root = true) {
     } else if (typeof(payload[key]) == "object" && payload[key].length == undefined){
       let newKey = getNewKey(key, name, root);
       newKey = root ? newKey : name + newKey;
-      children.push(generate(payload[key], newKey, indentationLevel, false));
+      children.push(generate(payload[key], newKey, indentationLevel, false, rootName));
       code.push("  ".repeat(indentationLevel) + `${key}: {type: ${newKey}, setter: false}`);
     } else if (typeof(payload[key]) == "object" && payload[key].length != undefined) {
       // ARRAY
       let newKey = getNewKey(key, name, root, true);
-      children.push(generate(payload[key][0], newKey, indentationLevel, false));
+      children.push(generate(payload[key][0], newKey, indentationLevel, false, rootName));
       code.push("  ".repeat(indentationLevel) + `${key}: {type: Array(${newKey}), setter: false}`);
     } else {
       code.push("  ".repeat(indentationLevel) + `${key}: {type: ${keyMappings[typeof(payload[key])]}, setter: false}`);
@@ -77,14 +77,104 @@ function generate(payload, name, indentationLevel = 2, root = true) {
 }
 
 const JSON = {
-  "ref": "refs/heads/featureA",
-  "node_id": "MDM6UmVmcmVmcy9oZWFkcy9mZWF0dXJlQQ==",
-  "url": "https://api.github.com/repos/octocat/Hello-World/git/refs/heads/featureA",
-  "object": {
-    "type": "commit",
-    "sha": "aa218f56b14c9653891f9e74264a383fa43fefbd",
-    "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/aa218f56b14c9653891f9e74264a383fa43fefbd"
-  }
+  "sha": "388e61b6c707367aca2b9737766835f4cbe2cbb4",
+  "node_id": "MDY6Q29tbWl0NTY1MTAyMTk6Mzg4ZTYxYjZjNzA3MzY3YWNhMmI5NzM3NzY2ODM1ZjRjYmUyY2JiNA==",
+  "commit": {
+    "author": {
+      "name": "SinisterRectus",
+      "email": "sinister.rectus@gmail.com",
+      "date": "2020-03-06T17:09:36Z"
+    },
+    "committer": {
+      "name": "SinisterRectus",
+      "email": "sinister.rectus@gmail.com",
+      "date": "2020-03-06T17:09:36Z"
+    },
+    "message": "Fixes ratelimit header lookup",
+    "tree": {
+      "sha": "4dc8c11d8c1a0e588a7a05ce103fcf5ca3e63a0d",
+      "url": "https://api.github.com/repos/SinisterRectus/Discordia/git/trees/4dc8c11d8c1a0e588a7a05ce103fcf5ca3e63a0d"
+    },
+    "url": "https://api.github.com/repos/SinisterRectus/Discordia/git/commits/388e61b6c707367aca2b9737766835f4cbe2cbb4",
+    "comment_count": 0,
+    "verification": {
+      "verified": false,
+      "reason": "unsigned",
+      "signature": null,
+      "payload": null
+    }
+  },
+  "url": "https://api.github.com/repos/SinisterRectus/Discordia/commits/388e61b6c707367aca2b9737766835f4cbe2cbb4",
+  "html_url": "https://github.com/SinisterRectus/Discordia/commit/388e61b6c707367aca2b9737766835f4cbe2cbb4",
+  "comments_url": "https://api.github.com/repos/SinisterRectus/Discordia/commits/388e61b6c707367aca2b9737766835f4cbe2cbb4/comments",
+  "author": {
+    "login": "SinisterRectus",
+    "id": 11720541,
+    "node_id": "MDQ6VXNlcjExNzIwNTQx",
+    "avatar_url": "https://avatars1.githubusercontent.com/u/11720541?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/SinisterRectus",
+    "html_url": "https://github.com/SinisterRectus",
+    "followers_url": "https://api.github.com/users/SinisterRectus/followers",
+    "following_url": "https://api.github.com/users/SinisterRectus/following{/other_user}",
+    "gists_url": "https://api.github.com/users/SinisterRectus/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/SinisterRectus/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/SinisterRectus/subscriptions",
+    "organizations_url": "https://api.github.com/users/SinisterRectus/orgs",
+    "repos_url": "https://api.github.com/users/SinisterRectus/repos",
+    "events_url": "https://api.github.com/users/SinisterRectus/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/SinisterRectus/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  "committer": {
+    "login": "SinisterRectus",
+    "id": 11720541,
+    "node_id": "MDQ6VXNlcjExNzIwNTQx",
+    "avatar_url": "https://avatars1.githubusercontent.com/u/11720541?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/SinisterRectus",
+    "html_url": "https://github.com/SinisterRectus",
+    "followers_url": "https://api.github.com/users/SinisterRectus/followers",
+    "following_url": "https://api.github.com/users/SinisterRectus/following{/other_user}",
+    "gists_url": "https://api.github.com/users/SinisterRectus/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/SinisterRectus/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/SinisterRectus/subscriptions",
+    "organizations_url": "https://api.github.com/users/SinisterRectus/orgs",
+    "repos_url": "https://api.github.com/users/SinisterRectus/repos",
+    "events_url": "https://api.github.com/users/SinisterRectus/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/SinisterRectus/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  "parents": [
+    {
+      "sha": "8ad5334d8fe27223383cc98a1941b31f0fa113e8",
+      "url": "https://api.github.com/repos/SinisterRectus/Discordia/commits/8ad5334d8fe27223383cc98a1941b31f0fa113e8",
+      "html_url": "https://github.com/SinisterRectus/Discordia/commit/8ad5334d8fe27223383cc98a1941b31f0fa113e8"
+    }
+  ],
+  "stats": {
+    "total": 8,
+    "additions": 4,
+    "deletions": 4
+  },
+  "files": [
+    {
+      "sha": "ff06915c2aa905d35c5072246a16cfbe6109ca29",
+      "filename": "libs/client/API.lua",
+      "status": "modified",
+      "additions": 4,
+      "deletions": 4,
+      "changes": 8,
+      "blob_url": "https://github.com/SinisterRectus/Discordia/blob/388e61b6c707367aca2b9737766835f4cbe2cbb4/libs/client/API.lua",
+      "raw_url": "https://github.com/SinisterRectus/Discordia/raw/388e61b6c707367aca2b9737766835f4cbe2cbb4/libs/client/API.lua",
+      "contents_url": "https://api.github.com/repos/SinisterRectus/Discordia/contents/libs/client/API.lua?ref=388e61b6c707367aca2b9737766835f4cbe2cbb4",
+      "patch": "@@ -187,15 +187,15 @@ function API:commit(method, url, req, payload, retries)\n \tend\n \n \tfor i, v in ipairs(res) do\n-\t\tres[v[1]] = v[2]\n+\t\tres[v[1]:lower()] = v[2]\n \t\tres[i] = nil\n \tend\n \n-\tif res['X-RateLimit-Remaining'] == '0' then\n-\t\tdelay = max(1000 * res['X-RateLimit-Reset-After'], delay)\n+\tif res['x-ratelimit-remaining'] == '0' then\n+\t\tdelay = max(1000 * res['x-ratelimit-reset-after'], delay)\n \tend\n \n-\tlocal data = res['Content-Type'] == JSON and decode(msg, 1, null) or msg\n+\tlocal data = res['content-type'] == JSON and decode(msg, 1, null) or msg\n \n \tif res.code < 300 then\n "
+    }
+  ]
 }
 
-console.log(generate(JSON, "Ref", 2, true));
+let name = "Commit"
+console.log(`struct ${name} do\nend\n\n`);
+console.log(generate(JSON, name, 2, true, name));
